da599e09464baacffa58230638379ee4f8af507a
Marcelo Mendes <marcelotavares.96@gmail.com>
2025-01-23 13:13:15 -0300

    perf(logica): separa lógica das operações

---
+++ src/controllers/calculatorMultiplyController.js
@@ -0,0 +1,13 @@
+exports.multiply = (req, res) => {
+  const { number1, number2 } = req.body;
+
+  if (!number1 || !number2) {
+    return res
+      .status(400)
+      .json({ error: "Todos os parametros são obrigatórios" });
+  }
+
+  const resultado = number1 * number2;
+
+  res.json({ resultado });
+};
