328246bbdc9c30f50d30439ace7908bbd0519bff
Marcelo Mendes <marcelotavares.96@gmail.com>
2025-01-23 13:14:57 -0300

    perf(rotas): atualiza rotas

--- src/routes/calculatorRoutes.js
+++ src/routes/calculatorRoutes.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const calculatorController = require("../controllers/calculatorController");
+const calculatorAddController = require("../controllers/calculatorAddController");
+const calculatorDivideController = require("../controllers/calculatorDivideController");
+const calculatorMultiplyController = require("../controllers/calculatorMultiplyController");
+const calculatorSubtractController = require("../controllers/calculatorSubtractController");
 
-router.post("/", calculatorController.calculate);
+router.post("/add", calculatorAddController.add);
+router.post("/divide", calculatorDivideController.divide);
+router.post("/multiply", calculatorMultiplyController.multiply);
+router.post("/subtract", calculatorSubtractController.subtract);
 
 module.exports = router;
